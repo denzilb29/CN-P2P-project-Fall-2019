420037b79da6b4972d220f7c27acc2037d0089dd
eeshjoshi <eesh.joshi@gmail.com>
2019-12-04 19:09:35 -0500

    1st

--- Peer.java
+++ Peer.java
@@ -4,25 +4,14 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.*;
 
-
 abstract class Peer {
 
-    //MultithreadedP mp = new MultithreadedP();
-
     public final int BUFFER_SIZE = 102400; //100kB
-
     public final static int TOTAL_PEERS = 5;
-
     public static String peerName = "";
-
     public abstract void Start();
-
     public static HashMap<Integer, byte[]> chunkList = new HashMap<Integer, byte[]>();
-
     public static ArrayList<Integer> chunkIndex = new ArrayList<Integer>();
-
     Peer() {
-
     }
-
 }

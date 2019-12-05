b344208253f4e5c0c6afa9b516c617bd30f95e17
eeshjoshi <eesh.joshi@gmail.com>
2019-12-04 19:01:41 -0500

    1st

---
+++ MultithreadedC.java
@@ -0,0 +1,94 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class MultithreadedC extends SocketThread {
+    //test
+    protected int pid;
+    //hello
+    public void setPid(int id) {
+        this.pid = id;
+    }
+
+    @Override
+    public void run() {
+        for (;;) {
+            try {
+                System.out.println("[" + this.peerName + "] Peer is listening command:");
+                Object packet = this.iStream.readObject();
+                assert (packet instanceof String);
+                String msg = (String) packet;
+                System.out.println("[" + this.peerName + "] Received message (" + msg + ")");
+                int readIndex = -1;
+                if(msg.compareTo("LIST") == 0){
+                    ArrayList<Integer> list = new ArrayList<Integer>(this.workingChunk.size());
+                    for (Integer v : this.workingChunk.keySet()) {
+                        list.add(v);
+                    }
+                    send(list);
+                }
+                else if(msg.compareTo("REQUEST") == 0){
+                    readIndex = this.iStream.readInt();
+                    send(readIndex);
+                    send(this.workingChunk.get(readIndex));
+                }
+                else if(msg.compareTo("ASK") == 0){
+                    readIndex = this.iStream.readInt();
+                    if (!this.workingChunk.containsKey(readIndex)) {
+                        send(0);
+                    }
+                    else {
+                        send(1);
+                    }
+                }
+                else if(msg.compareTo("DATA") == 0){
+                    readIndex = this.iStream.readInt();
+                    byte[] piece = (byte[]) this.iStream.readObject();
+                    int t=0;
+                    if (this.workingChunk.containsKey(readIndex)) {
+                        ++t;
+                    }
+                    else{
+                        workingChunk.put(readIndex, piece);
+                        System.out.println("Received Chunk #" + readIndex);
+                        storePiece(readIndex, piece);
+                    }
+                }
+                else if(msg.compareTo("CLOSE") == 0){
+                    oStream.close();
+                    iStream.close();
+                    return;
+                }
+            }
+            catch (ClassNotFoundException | IOException exceptions) {
+                exceptions.printStackTrace();
+                System.out.println("[" + this.getName() + "]: Session ended.");
+                return;
+            }
+        }
+    }
+
+    private void storePiece(int x, byte[] chunk) {
+        try {
+            FileOutputStream fos = new FileOutputStream("PEER" + this.pid + "Dir/" + x, false);
+            fos.write(chunk);
+            fos.flush();
+            fos.close();
+        }
+        catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+}
--- Server.java
+++ Server.java
@@ -13,100 +13,11 @@ import java.io.PrintWriter;
 import java.io.BufferedReader; 
 import java.io.InputStreamReader;
 
-class MultithreadedS extends SocketThread {
-    
-    protected int cid = -1;
-    
-    private int generatePID() {
-        int count=0;
-        while(count < 100){
-            if (Server.peerConfig.containsKey(count) && (!Server.peerList.containsKey(count))) {
-                Server.peerList.put(count, (Server.peerConfig.get(count)).get(0));
-                return count;
-            }
-            ++count;
-        }
-        return -1;
-    }
-
-    
-    public void run() {
-        for(;;) {
-            try {
-                System.out.println("[Server] Server is listening command:");
-                Object obj;
-                for(;;) {
-                    try {
-                        obj = this.iStream.readObject();
-                        assert(obj instanceof String);
-                        break;
-                    } catch (Exception ignored) {
-
-                    }
-                }
-                String m = (String) obj;
-                System.out.println("[Server] Received message (" + m + ") from " + cid);
-                int var1 = -1;
-                if (m.compareTo("LIST")==0) {
-                    ArrayList<Integer> list1 = new ArrayList<Integer>(this.workingChunk.size());
-                    int i=0;
-                    while(i < this.workingChunk.size()){
-                        if(this.workingChunk.containsKey(i)){
-                            list1.add(i);
-                        }
-                        ++i;
-                    }
-                    send(list1);
-                }
-                else if (m.compareTo("NAME")==0) {
-                    send((Object) this.fileName);
-                }
-                else if(m.compareTo("REQUEST")==0) {
-                    var1 = this.iStream.readInt();
-                    send(var1);
-                    send(this.workingChunk.get(var1));
-                }
-                else if(m.compareTo("DATA")==0) {
-                    var1 = this.iStream.readInt();
-                    byte[] chunk = (byte[]) this.iStream.readObject();
-                }
-                else if(m.compareTo("REGISTER")==0) {
-                    int x = this.generatePID();
-                    int y = Server.peerList.get(x);
-                    send(x);
-                    send(y);
-                }
-                else if(m.compareTo("PEER")==0) {
-                    System.out.print("[Server] Peer list:");
-                    int cpid = iStream.readInt();
-                    for (int i : Server.peerList.keySet()) {
-                        System.out.print(i + " ");
-                    }
-                    System.out.println();
-                    System.out.println("[Server] Send U/D neighbor to clients");
-                    send(Server.peerList);
-                    send((Object) (Server.peerConfig.get(cpid)).get(1));
-                    send((Object) (Server.peerConfig.get(cpid)).get(2));
-                }
-                else if(m.compareTo("CLOSE")==0) {
-                    oStream.close();
-                    iStream.close();
-                    Server.peerList.remove(cid);
-                    return;
-                }
-            }
-            catch (ClassNotFoundException | IOException ioexception) {
-                ioexception.printStackTrace();
-                System.out.println("[" + this.getName() + "]: Session ended.");
-                Server.peerList.remove(cid);
-                return;
-            }
-        }
-    }
-}
 
 // This is the file owner server
 public class Server extends Peer{
+
+    MultithreadedS ms = new MultithreadedS();
 
     private String val = "Server";
     String wd = System.getProperty("user.dir");
